f3bf81fd80733d6a4503e785bf5320e27fad35e6
cwork <colbycwork@gmail.com>
2020-10-16 21:20:58 -0700

    Change class to className

--- src/components/page/PageTitle.js
+++ src/components/page/PageTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PageTitle = ({ title }) => {
-  return <h1 class="has-size-2 page-title">{title}</h1>;
+  return <h1 className="has-size-2 page-title">{title}</h1>;
 };
 
 export default PageTitle;
