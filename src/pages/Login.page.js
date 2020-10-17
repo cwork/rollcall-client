489689ea755f07a30e5684bbf54286fe0faa88a5
cwork <colbycwork@gmail.com>
2020-10-16 20:52:42 -0700

    Add classes for styling login page

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -19,29 +19,31 @@ const LoginPage = () => {
 
   return (
     <PageContainer>
-      <Main title="Sign In">
-        <form onSubmit={handleSubmit}>
-          <Input
-            type="text"
-            value={identifierValue}
-            label="Username or Email"
-            id="identifier"
-            name="identifier"
-            placeholder="Enter a username or email..."
-            onChange={handleIdentifierChange}
-          />
-          <Input
-            type="password"
-            value={passwordValue}
-            label="Password"
-            id="password"
-            name="password"
-            placeholder="Enter your super spicy secret password ..."
-            onChange={handlePasswordChange}
-          />
-          <Button val="Sign in" color="success" />
-        </form>
-      </Main>
+      <div className="columns is-centered is-vcentered is-mobile login-page-columns">
+        <Main title="Sign In" colSpan="half">
+          <form onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              value={identifierValue}
+              label="Username or Email"
+              id="identifier"
+              name="identifier"
+              placeholder="Enter a username or email..."
+              onChange={handleIdentifierChange}
+            />
+            <Input
+              type="password"
+              value={passwordValue}
+              label="Password"
+              id="password"
+              name="password"
+              placeholder="Enter your super spicy secret password ..."
+              onChange={handlePasswordChange}
+            />
+            <Button val="Sign in" color="success" />
+          </form>
+        </Main>
+      </div>
     </PageContainer>
   );
 };
