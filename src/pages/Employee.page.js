aa36a5407c710a5da71a6c6528f42fd58a291ea0
cwork <colbycwork@gmail.com>
2020-10-17 10:22:56 -0700

    Add Employee detail page

--- src/pages/Employee.page.js
+++ src/pages/Employee.page.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PageContainer from '../components/page/PageContainer';
+import Main from '../components/page/MainContent';
+import Aside from '../components/page/Aside';
+import Axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-const EmployeePage = ({ employee }) => {
-  return <h1>Employee</h1>;
+const EmployeePage = () => {
+  const employeeId = useParams().employeeId;
+  const [employee, setEmployee] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const employee = await Axios.get(
+          `http://localhost:5000/api/employee/${employeeId}`
+        );
+        setEmployee(employee.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log('error', error.response);
+      }
+    };
+    fetchData();
+  }, [isLoading, employeeId]);
+
+  const employeeName = `${employee.firstName} ${employee.lastName}`;
+
+  return (
+    <PageContainer>
+      <Main title={employeeName}></Main>
+    </PageContainer>
+  );
 };
 
 export default EmployeePage;
