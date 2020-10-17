c63753516236d7044530d63ae017ea58ddad2005
cwork <colbycwork@gmail.com>
2020-10-16 21:32:32 -0700

    Inverse conditions and add a comment

--- src/apps/AppController.js
+++ src/apps/AppController.js
@@ -3,10 +3,15 @@ import authContext from '../contexts/auth/authContext';
 import IsAuthenticated from './IsAuthenticated';
 import IsNotAuthenticated from './IsNotAuthenticated';
 
+/**
+ * This AppController will be imported in the main App component.
+ * It will get the current auth state, and will conditionally
+ * load components based on whether a user is logged in or not.
+ */
 const AppController = () => {
   const { isAuthenticated } = useContext(authContext);
-  if (isAuthenticated) return <IsAuthenticated />;
-  return <IsNotAuthenticated />;
+  if (!isAuthenticated) return <IsNotAuthenticated />;
+  return <IsAuthenticated />;
 };
 
 export default AppController;
