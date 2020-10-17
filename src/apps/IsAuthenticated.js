ff17b31de33f2496c66a0801ac83de009e8230ba
cwork <colbycwork@gmail.com>
2020-10-17 09:58:14 -0700

    Routing

--- src/apps/IsAuthenticated.js
+++ src/apps/IsAuthenticated.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EmployeesPage from '../pages/Employees.page';
+import EmployeePage from '../pages/Employee.page';
 
 const IsAuthenticated = () => {
-  return <EmployeesPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={EmployeesPage} />
+        <Route exact path="/employee/:employeeId" component={EmployeePage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default IsAuthenticated;
