b6d56cb7073fecff019695015ea4987663877997
cwork <colbycwork@gmail.com>
2020-10-21 23:42:19 -0700

    Change styling

--- src/components/employee/ActiveControl.js
+++ src/components/employee/ActiveControl.js
@@ -3,15 +3,23 @@ import React from 'react';
 const ActiveControl = ({ showAll, handleInputChange }) => {
   return (
     <form className="p-2 has-background-warning">
-      <label htmlFor="toggle" className="label">
-        Hide inactive employees
-      </label>
-      <input
-        type="checkbox"
-        className="checkbox"
-        checked={!showAll}
-        onChange={handleInputChange}
-      />
+      <div className="field is-horizontal">
+        <div className="field-label">
+          <label htmlFor="toggle" className="label">
+            Hide inactive
+          </label>
+        </div>
+        <div className="field-body">
+          <div>
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={!showAll}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+      </div>
     </form>
   );
 };
