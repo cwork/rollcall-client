6262f357894bac68916d62b3a614f2e39b95f2eb
cwork <colbycwork@gmail.com>
2020-10-22 23:36:53 -0700

    Add effect to change document title

--- src/components/page/PageTitle.js
+++ src/components/page/PageTitle.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PageTitle = ({ title }) => {
+  useEffect(() => {
+    document.title = `RollCall | ${title}`;
+  }, [title]);
   return <h1 className="is-size-2 page-title mb-4">{title}</h1>;
 };
 
