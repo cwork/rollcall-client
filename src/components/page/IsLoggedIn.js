e87570496e894e81fd21716e67fd80a6dea8414f
cwork <colbycwork@gmail.com>
2020-10-23 21:22:06 -0700

    Add link to add user page

--- src/components/page/IsLoggedIn.js
+++ src/components/page/IsLoggedIn.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../form/Button';
 
 const IsLoggedIn = ({ logout }) => {
-  return <Button onClick={logout} val="Logout" />;
+  return (
+    <div className="buttons">
+      <Link to="/user/add" className="button">
+        Add User
+      </Link>
+      <Button onClick={logout} val="Logout" />
+    </div>
+  );
 };
 
 export default IsLoggedIn;
